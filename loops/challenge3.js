fca87dad365abd65a9bc9ba34dab4c54f295a1b6
codenation <codenation@codenations-MacBook-Pro.local>
2019-05-23 16:51:17 +0100

    completed array challenge 3

---
+++ loops/challenge3.js
@@ -0,0 +1,6 @@
+//Challenge 3 - Take the string from problem 2 and print each letter out individually.
+let jibberish = "jrfndklhgfndjkjlkgperfijfhidknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhiejkdsoiufghedjwsh"
+let jibArray = jibberish.split("")
+for (i=0;i<jibArray.length;i++){
+    console.log(jibArray[i])
+}
