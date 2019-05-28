cd5aade460f20857b6bcf089592cedde80d00382
codenation <codenation@codenations-MacBook-Pro.local>
2019-05-28 13:18:48 +0100

    changes to loop challenge 2 and 4

--- loops/challenge4.js
+++ loops/challenge4.js
@@ -1,7 +1,12 @@
 //Challenge 4 - Take the string from problem 2, loop through the string and find out how many times the word hi 
 //appears and the indexes where it appears.
 let jibberish = "jrfndklhgfndjkjlkgperfijfhidknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhiejkdsoiufghedjwsh"
-let indexOfLast = jibberish.lastIndexOf("hi")
-for (i=0;i<jibArray.length;i++){
-    console.log(jibArray[i])
-}
+let indexOfFirst = jibberish.indexOf("hi")
+let counter = 0
+
+ 
+
+
+
+
+console.log(indexOfFirst)
